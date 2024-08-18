4db2bdcc3e964284e1c4d9da9d9e0d305b5bc75c
RomaBez <bezvuglyak.roman@gmail.com>
2024-08-18 07:27:09 +0300

    fixed

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,10 +1,13 @@
+import { ReactElement } from "react";
 import css from "./LoadMoreBtn.module.css";
 
 interface LoadMoreBtnProps {
   onLoadMore: () => void;
 }
 
-export default function LoadMoreBtn({ onLoadMore }): LoadMoreBtnProps {
+export default function LoadMoreBtn({
+  onLoadMore,
+}: LoadMoreBtnProps): ReactElement {
   return (
     <button onClick={onLoadMore} className={css.LoadMoreBtn}>
       Load More
